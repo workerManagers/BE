98a42fd10d1c2514a5273ea241822d2f30853fde
jiva-z <jiva_z@naver.com>
2025-03-30 16:46:33 +0900

    chore: initialize project environment
    Initialized project structure and set up basic configurations.
    - Created initial directory layout
    - Added .gitignore and other essential configuration files

---
+++ src/main/java/com/example/workerManagers/WorkerManagersApplication.java
@@ -0,0 +1,13 @@
+package com.example.workerManagers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerManagersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerManagersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workerManagers/WorkerManagersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workerManagers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerManagersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
