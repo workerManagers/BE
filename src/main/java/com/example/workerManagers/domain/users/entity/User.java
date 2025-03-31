6ababfedde5f1e82be01764b2094abbcf85b60a4
kkh <rlarudgnsekd@naver.com>
2025-04-01 04:30:27 +0900

    feat: create essential tables

---
+++ src/main/java/com/example/workerManagers/domain/users/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.workerManagers.domain.users.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user") // 데이터베이스 테이블 이름
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 자동 증가 설정
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_name", length = 20, nullable = false)
+    private String userName;
+
+    @Column(name = "user_sex", length = 20)
+    private String userSex;
+
+    @Column(name = "user_age")
+    private Integer userAge;
+
+    @Column(name = "user_email", length = 20)
+    private String userEmail;
+
+    // Getters and Setters (생략 가능)
+}
