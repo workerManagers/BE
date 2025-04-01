b03c96abc56bc9a29a135c39b435864524cc8a94
kkh <rlarudgnsekd@naver.com>
2025-04-01 20:45:34 +0900

    feat: add user_pass in user DB

--- src/main/java/com/example/workerManagers/domain/users/entity/User.java
+++ src/main/java/com/example/workerManagers/domain/users/entity/User.java
@@ -14,16 +14,19 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
 
+    @Column(name = "user_pass", length = 20, nullable = false)
+    private String userPass;
+
     @Column(name = "user_name", length = 20, nullable = false)
     private String userName;
 
-    @Column(name = "user_sex", length = 20)
+    @Column(name = "user_sex", length = 20, nullable = false)
     private String userSex;
 
-    @Column(name = "user_age")
+    @Column(name = "user_age", nullable = false)
     private Integer userAge;
 
-    @Column(name = "user_email", length = 20)
+    @Column(name = "user_email", length = 20, nullable = false)
     private String userEmail;
 
     // Getters and Setters (생략 가능)
