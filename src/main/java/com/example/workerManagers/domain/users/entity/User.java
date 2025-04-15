35192ad25b853dc266c73ab88df37e3c7aa39cf6
jiva-z <jiva_z@naver.com>
2025-04-15 12:03:18 +0900

    Update environment variables and deployment settings

--- src/main/java/com/example/workerManagers/domain/users/entity/User.java
+++ src/main/java/com/example/workerManagers/domain/users/entity/User.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity
-@Table(name = "user") // 데이터베이스 테이블 이름
+@Table(name = "users") // 데이터베이스 테이블 이름
 @Getter
 @Setter
 @NoArgsConstructor
